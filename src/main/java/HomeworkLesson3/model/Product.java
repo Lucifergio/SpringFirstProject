d9662e1f81fac7040bfdac454f6933278bff024b
aleksey <lexasalnikov@gmail.com>
2022-04-01 13:10:41 +0500

    HomeWork Lesson3

---
+++ src/main/java/HomeworkLesson3/model/Product.java
@@ -0,0 +1,29 @@
+package HomeworkLesson3.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    private Integer id;
+    private String title;
+    private int cost;
+
+    public Product(int id, String title, int cost) {
+        this.id = id;
+        this.title = title;
+        this.cost = cost;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", cost=" + cost +
+                '}';
+    }
+}
